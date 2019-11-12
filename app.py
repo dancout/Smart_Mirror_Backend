f20d0e82c9bcb878c71f902971ee0f0d19d46ef0
Daniel Couturier <dancout@umich.edu>
2019-11-12 16:03:29 -0500

    Calendar route (#2)
    * Initial Calendar route setup
    
    * /calendar route grabs today's events and the following 2 days

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, Response
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime, timezone, timedelta
+import urllib.parse
+
 
 # ========================= Initial Setup =========================
 # - Name this flast app
@@ -9,6 +12,14 @@ app = Flask(__name__)
 
 # - Load in the environment variables from the .env file
 load_dotenv()
+
+
+# ========================= Helper Functions ================================
+def formatTime(t):
+    t = t.split("T")[0]
+    t += "T00:00:00-05:00"  # TODO: do we need to drop this -5?
+    return t
+
 
 # ========================= Routes ================================
 """
@@ -47,5 +58,52 @@ def weather():
 
     response = requests.request(
         "GET", url, headers=headers, params=querystring)
+
+    return response.text
+
+
+"""
+This route returns a user's [google] calendar events from today and the following 2 days
+"""
+@app.route('/calendar')
+def calendar():
+
+    # Grab the current time
+    current_time = datetime.today()
+    # Grab this timestamp, in 3 days
+    future_time = current_time + timedelta(days=3)
+
+    # Set the current and 3 days future timestamps to a readable format
+    current_time_str = formatTime(current_time.isoformat())
+    future_time_str = formatTime(future_time.isoformat())
+
+    # Grab your primary google calendar and google api key from the .env file
+    google_primary_calendar = os.getenv(
+        "google_primary_calendar", "not available")
+    google_api_key = os.getenv("google_api_key", "not available")
+
+    # Set the base url
+    url = f"https://www.googleapis.com/calendar/v3/calendars/{google_primary_calendar}/events?"
+
+    # Set the necessary params for the GET request
+    maxResults = 999
+    orderBy = "startTime"
+    singleEvents = "true"
+
+    params = {
+        "orderBy": orderBy,
+        "maxResults": maxResults,
+        "singleEvents": singleEvents,
+        "timeMin": current_time_str,
+        "timeMax": future_time_str,
+        "key": google_api_key,
+    }
+
+    # Add the params to the GET Request url
+    url += urllib.parse.urlencode(
+        params, quote_via=urllib.parse.quote)
+
+    response = requests.request(
+        "GET", url)
 
     return response.text
