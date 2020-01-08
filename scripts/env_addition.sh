8d7e34b138143d314a92d3003219e12193baf97e
Daniel Couturier <dancout@umich.edu>
2020-01-07 20:53:32 -0500

    env_addition.sh hotfix (#8)
    Added '.env' to the end of the line regarding ~where~ to write this line entry.

--- scripts/env_addition.sh
+++ scripts/env_addition.sh
@@ -3,7 +3,7 @@ my_addition=$1
 
 env_addition () {
     echo "Adding '$1' to your .env file"
-    echo "export $1='Please_Replace_Me_With_Proper_Key'" | sudo tee -a ~/Smart_Mirror_Backend
+    echo "export $1='Please_Replace_Me_With_Proper_Key'" | sudo tee -a ~/Smart_Mirror_Backend/.env
 }
 
 env_addition $my_addition
